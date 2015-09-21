c45e17ef3b6b4aaec3f50dfd4cfb0bbf2c37a7a4
Ian Finlayson <finlaysoni@gmail.com>
2015-09-21 12:47:30 -0400

    Added C file and Makefile

---
+++ Makefile
@@ -0,0 +1,22 @@
+# Makefile
+
+CC=clang
+FLAGS=-g -W -Wall
+TARGET=png2gba
+ 
+# do everything
+all: tags $(TARGET)
+	@echo "All done!"
+
+# link it all together
+$(TARGET): png2gba.c
+	$(CC) $(FLAGS) -o $(TARGET) png2gba.c
+ 
+# tidy up
+clean:
+	rm -f $(TARGET) tags
+
+# create ctags index
+tags:
+	ctags png2gba.c
+
---
+++ png2gba.c
@@ -0,0 +1,20 @@
+/* png2gba.c
+ * this program converts PNG images into C header files storing
+ * arrays of data as required for programming the GBA */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define PNG_DEBUG 3
+#include <png.h>
+
+int main( ) {
+
+
+
+    return 0;
+}
+
